b540bc22dddde754480a09fcab478474e274bd53
Rafaela Almeida <50708203+rafsalmeida@users.noreply.github.com>
2020-03-19 12:55:06 +0000

    Add project files.

---
+++ Pmviz_Frontend/Controllers/LogController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PmvizFrontend.Models;
+
+namespace PmvizFrontend.Controllers
+{
+    public class LogController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Log> logList = new List<Log>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:8080/api/logs"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    logList = JsonConvert.DeserializeObject<List<Log>>(apiResponse);
+                }
+            }
+            return View(logList);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Pmviz_Frontend/Models/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace PmvizFrontend.Models
+{
+    [DataContract]
+    public class Log
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public string EndDate { get; set; }
+        [DataMember]
+        public string Filename { get; set; }
+        [DataMember]
+        public int NumberActivities { get; set; }
+        [DataMember]
+        public int NumberCases { get; set; }
+        [DataMember]
+        public string StartDate { get; set; }
+    }
+}
