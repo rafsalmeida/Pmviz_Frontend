a062497e855c21f041c386c1657fcfabde0c4a1e
Rafaela Almeida <50708203+rafsalmeida@users.noreply.github.com>
2020-03-20 15:58:20 +0000

    Initial UI for Login

---
+++ Pmviz_Frontend/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Pmviz_Frontend.Models
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public string Username { get; set; }
+
+        [DataMember]
+        public int Id_Role { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int Email { get; set; }
+    }
+}
